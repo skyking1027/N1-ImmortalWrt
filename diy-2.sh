086ebbc020f397c236b30cba8b73ac25459a30d0
skyking1027 <61034437+skyking1027@users.noreply.github.com>
2026-03-18 19:01:39 +0800

    Update diy-2.sh

--- diy-2.sh
+++ diy-2.sh
@@ -5,6 +5,7 @@ git clone --depth=1 https://github.com/ophub/luci-app-amlogic package/luci-app-a
 git clone --depth=1 https://github.com/OldCoding/luci-app-filebrowser package/luci-app-filebrowser
 git clone --depth=1 https://github.com/jerrykuku/luci-app-argon-config.git package/luci-app-argon-config
 git clone --depth=1 https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon
+git clone --depth=1 https://github.com/vernesong/OpenClash package/OpenClash
 # 修改默认主题
 sed -i 's/luci-theme-design/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
 sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
