0fc55ce86135f19ccf2eccbcbc5821a2d897c8b2
skyking1027 <61034437+skyking1027@users.noreply.github.com>
2026-03-18 12:55:50 +0800

    Update diy-1.sh

--- diy-1.sh
+++ diy-1.sh
@@ -2,3 +2,5 @@
 
 #添加feeds源
 echo "src-git nikki https://github.com/nikkinikki-org/OpenWrt-nikki.git;main" >> feeds.conf.default
+echo 'src-git nas https://github.com/linkease/nas-packages.git;master' >> feeds.conf.default
+echo 'src-git nas_luci https://github.com/linkease/nas-packages-luci.git;main' >> feeds.conf.default
