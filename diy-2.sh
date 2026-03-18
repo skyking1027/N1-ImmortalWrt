0ae6a005cda34820c72cd620a9302805f1287d5e
skyking1027 <61034437+skyking1027@users.noreply.github.com>
2026-03-18 13:03:42 +0800

    Update diy-2.sh

--- diy-2.sh
+++ diy-2.sh
@@ -3,3 +3,5 @@
 # 添加包
 git clone --depth=1 https://github.com/ophub/luci-app-amlogic package/luci-app-amlogic
 git clone --depth=1 https://github.com/OldCoding/luci-app-filebrowser package/luci-app-filebrowser
+git clone --depth=1 https://github.com/jerrykuku/luci-app-argon-config.git package/luci-app-argon-config
+git clone --depth=1 https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon
