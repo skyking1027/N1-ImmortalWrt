04f1a2d17d6d6fbd026911182bcc48f8acb447b6
skyking1027 <61034437+skyking1027@users.noreply.github.com>
2026-03-18 12:56:40 +0800

    Update diy-2.sh

--- diy-2.sh
+++ diy-2.sh
@@ -2,3 +2,4 @@
 
 # 添加包
 git clone --depth=1 https://github.com/ophub/luci-app-amlogic package/luci-app-amlogic
+git clone --depth=1 https://github.com/OldCoding/luci-app-filebrowser package/luci-app-filebrowser
