943620aa4bda240d7c900a50f81ee81e4086ff17
skyking1027 <61034437+skyking1027@users.noreply.github.com>
2026-03-26 06:04:48 +0800

    Update diy-1.sh

--- diy-1.sh
+++ diy-1.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 #添加feeds源
-echo "src-git nikki https://github.com/nikkinikki-org/OpenWrt-nikki.git;main" >> feeds.conf.default
-#echo "src-git kiddin9 https://github.com/kiddin9/op-packages.git;main" >> feeds.conf.default
+#echo "src-git nikki https://github.com/nikkinikki-org/OpenWrt-nikki.git;main" >> feeds.conf.default
+echo "src-git kiddin9 https://github.com/kiddin9/op-packages.git;main" >> feeds.conf.default
