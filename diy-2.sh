fdfd5f0dd7d36f523944c0bf64aed1f48939bcf0
skyking1027 <61034437+skyking1027@users.noreply.github.com>
2026-03-26 17:41:00 +0800

    Update diy-2.sh

--- diy-2.sh
+++ diy-2.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 # 添加包
-git clone --depth=1 https://github.com/ophub/luci-app-amlogic package/luci-app-amlogic
-git clone --depth=1 https://github.com/vernesong/OpenClash package/OpenClash
+#git clone --depth=1 https://github.com/ophub/luci-app-amlogic package/luci-app-amlogic
+#git clone --depth=1 https://github.com/vernesong/OpenClash package/OpenClash
 sed -i 's/^download-ci-llvm[[:space:]]*=.*/download-ci-llvm = false/' feeds/packages/lang/rust/bootstrap.toml 2>/dev/null || echo -e '[llvm]\ndownload-ci-llvm = false' > feeds/packages/lang/rust/bootstrap.toml
